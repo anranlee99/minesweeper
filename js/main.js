ec5834f95b247283f99b261c727d872508eb2a44
anranlee99 <anranlee99@gmail.com>
2022-09-15 22:04:20 -0700

    fixed flagging and implemented a way to generate bombs

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 /*----- constants -----*/
 
-let xAxis = 21;
-let yAxis = 10;
+let xAxis = 9;
+let yAxis = 9;
 let tileSize = '30px 30px';
 let seconds = 0;
-
+let bombCount = 10;
+let flagCount = 0;
+let bombCoords = {}
+let noCornerMode = false;
 /*----- app's state (variables) -----*/
 
 /*----- cached element references -----*/
@@ -26,42 +29,73 @@ resetBtnEl.addEventListener('mouseup', function() {
    
 
 boardEl.addEventListener('mousedown', function(evt) {
-    evt.target.style.background = `url('assets/pressed_tile.png')`;
-    evt.target.style.backgroundSize = '30px 30px';
-    console.log("board clicked")
+    switch (evt.which) {
+        case 1:
+            /* LMB DOWN CLICK */
+            //If it's not a flagged tile, 
+            if(!evt.target.classList.contains('flagged')){
+                setImg('pressed_tile.png', evt)
+            }
+            break;
+        case 2:
+            //console.log('Middle Mouse button pressed.');
+            break;
+        case 3:
+            /* RMB DOWN CLICK */
+            
+            //flag
+            if(evt.target.classList.contains('flagged'))
+            {
+                evt.target.classList.remove('flagged')
+                setImg('closed_tile.png', evt)
+                flagCount--;
+                setBombDisplay();
+                
+            } 
+            //unflag
+            else {
+                evt.target.classList.add('flagged')
+                setImg('flag_tile.png', evt)
+                flagCount++;
+                setBombDisplay();
+            }
+            
+            break;
+        default:
+            //console.log('You have a strange Mouse!');
+    }
+    
 })
 
 boardEl.addEventListener('mouseup', function(evt) {
-    evt.target.style.background = `url('./assets/closed_tile.png')`;
-    evt.target.style.backgroundSize = '30px 30px';
+    switch (evt.which) {
+        case 1:
+            //if tile is covered, uncover
+            //if the is tile is flagged or blank, do nothing
+            //if the tile is a number, do chord click
+            break;
+        case 2:
+            //console.log('Middle Mouse button pressed.');
+            break;
+        case 3:
+            //right mouse button
+            break;
+        default:
+            //console.log('You have a strange Mouse!');
+    }
 })
 
-boardEl.addEventListener('mouseleave', function(evt) {
-    evt.target.style.background = `url('./assets/closed_tile.png')`;
-    evt.target.style.backgroundSize = '30px 30px';
-})
+// boardEl.addEventListener('mouseleave', function(evt) {
+//     console.log('mouseleave')
+//     evt.target.style.background = `url('./assets/closed_tile.png')`;
+//     evt.target.style.backgroundSize = tileSize;
+// })
 
+//get rid of the menu on right click
 boardEl.addEventListener('contextmenu', function(evt) {
-    evt.target.style.background = `url('./assets/flag_tile.png')`;
-    evt.target.style.backgroundSize = '30px 30px';
     evt.preventDefault();
-  }, false);
+  });
 
-if (boardEl.addEventListener) {
-
-    boardEl.addEventListener('contextmenu', function(evt) {
-
-        evt.target.style.background = `url('./assets/flag_tile.png')`;
-        evt.target.style.backgroundSize = '30px 30px';
-        evt.preventDefault();
-
-    }, false);
-
-} else {
-    boardEl.attachEvent('oncontextmenu', function() {
-        window.event.returnValue = false;
-    });
-}
 
 /*----- functions -----*/
 
@@ -71,7 +105,7 @@ function generateGrids(){
             //TODO: fix the id generation
             let tile = document.createElement('div');
             tile.classList.add('tile')
-            console.log('i: ', i, 'j: ', j, `${i}${j}`)
+            tile.classList.add('covered')
             tile.id = `${i},${j}`
             tile.style.backgroundColor = 'pink';
             tile.style.height = '30px';
@@ -84,7 +118,15 @@ function generateGrids(){
     }
     document.querySelector('#board').style.width = `${30*xAxis + 2*xAxis}px`; 
     document.querySelector('#board').style.height = `${30*yAxis + 2*yAxis}px`;
-    document.querySelector('#statusBar').style.width = `${30*xAxis + 2*xAxis}px`
+    document.querySelector('#statusBar').style.width = `${30*xAxis + 2*xAxis}px`;
+
+    let statusBarHeight;
+    if(8*yAxis >= 90){
+        statusBarHeight = yAxis*8;
+    }   else{
+        statusBarHeight = 90;
+    }
+    document.querySelector('#statusBar').style.height = `${statusBarHeight}px`;
 
 }
 
@@ -107,9 +149,55 @@ function clock(){
     hundsEl.src = `./assets/d${hundreds}.svg`
 
 }
+
+function setBombDisplay(){
+    //don't use actual bomb count here, don't want to mess with calculations
+    let displayBombs = bombCount - flagCount
+    if(displayBombs<0)
+    {
+        displayBombs = 0;
+    }
+    let ones = Math.floor(displayBombs % 10)
+    let tens = Math.floor(displayBombs/10 % 10)
+    let hundreds = Math.floor(displayBombs/100 % 10)
+
+    let onesEl = document.querySelector('#bomb1s');
+    let tensEl = document.querySelector('#bomb10s');
+    let hundsEl = document.querySelector('#bomb100s');
+
+    onesEl.src = `./assets/d${ones}.svg`
+    tensEl.src = `./assets/d${tens}.svg`
+    hundsEl.src = `./assets/d${hundreds}.svg`
+}
+
+//helper function
+function setImg(img, evt){
+    evt.target.style.background = `url('./assets/${img}')`;
+    evt.target.style.backgroundSize = tileSize;
+}
+function generateBombs(){
+    //playing with bombs in corners
+    if(!noCornerMode){
+        while(Object.keys(bombCoords).length<bombCount){
+            let rand1 = Math.floor(Math.random() * (xAxis))
+            let rand2 = Math.floor(Math.random() * (yAxis))
+            
+            bombCoords[`${rand1},${rand2}`] = true
+        }
+    }
+
+    //playing without bombs in corners
+    while(Object.keys(bombCoords).length<bombCount){
+        let rand1 = Math.floor(Math.random() * (xAxis))
+        let rand2 = Math.floor(Math.random() * (yAxis))
+
+    if((rand1!==0 && rand1!==xAxis-1) && rand2!==0 && rand2!==yAxis-1)
+        bombCoords[`${rand1},${rand2}`] = true
+    }
+}
 //GAME START
 generateGrids();
 
-//setInterval(clock, 1000);
-
+// let refreshIntervalId = setInterval(clock, 1000);
+//clearInterval(refreshIntervalId);
 
