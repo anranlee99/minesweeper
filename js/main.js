e5f855082d340a77f18d17a674325125c88c906a
anranlee99 <anranlee99@gmail.com>
2022-09-16 19:59:30 -0700

    added lots of features and polished up old features

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 /*----- constants -----*/
-
-let xAxis = 20;
-let yAxis = 9;
+const MIN_DIM = 9;
+const MAX_DIM = 50
+const MIN_BOMB = 5;
+const MAX_BOMB = 25;
+let xAxis = MIN_DIM;
+let yAxis = MIN_DIM;
 let tileSize = '30px 30px';
 let seconds = 0;
 let bombCount = 10;
 let flagCount = 0;
-let bombCoords = {}
+let bombCoords = [];
 let noCornerMode = false;
+let refreshIntervalId = null;
+
+//not sure if i need this
+let newGame = false;
+
+
 /*----- app's state (variables) -----*/
 
 /*----- cached element references -----*/
@@ -15,19 +24,44 @@ let noCornerMode = false;
 const resetIconEl = document.querySelector('#resetIcon')
 const resetBtnEl = document.querySelector('#resetBtn');
 const boardEl = document.querySelector('#board');
-
+const inputBtnEl = document.querySelector('#customSettingsBtn')
 /*----- event listeners -----*/
 
+/*----- custom input btn listeners -----*/
+inputBtnEl.addEventListener('click', function(){
+    xAxis = parseInt(document.querySelector('#widthInput').value, 10);
+    yAxis = parseInt(document.querySelector('#heightInput').value, 10);
+    let bombPercent = parseInt(document.querySelector('#bombInput').value, 10);
+    //use a floor here to get an int back
+    bombCount = Math.floor(xAxis*yAxis*(bombPercent/100));
+    noCornerMode = document.querySelector('#noCorners').checked;
+    
+    
+    init();
+})
+/*----- custom input btn listeners -----*/
+/*----- reset button listeners -----*/
 resetBtnEl.addEventListener('mousedown', function() {
-    resetIconEl.src = 'assets/pressed_smiley.png'
-    console.log("reset clicked")
-})
+    resetIconEl.src = 'assets/pressed_smiley.png';
 
+    //stop the clock
+    clearInterval(refreshIntervalId);
+    //change clock images
+    seconds = 0;
+    document.querySelector('#time1s').src = `./assets/d0.svg`;
+    document.querySelector('#time10s').src = `./assets/d0.svg`;
+    document.querySelector('#time100s').src = `./assets/d0.svg`;
+    
+    
+})
 resetBtnEl.addEventListener('mouseup', function() {
-    resetIconEl.src = 'assets/unpressed_smiley.png'
+    resetIconEl.src = 'assets/unpressed_smiley.png';
+    init();
+    
 })
-   
+/*----- reset button listeners -----*/
 
+/*----- board listeners -----*/
 boardEl.addEventListener('mousedown', function(evt) {
     switch (evt.which) {
         case 1:
@@ -42,7 +76,6 @@ boardEl.addEventListener('mousedown', function(evt) {
             break;
         case 3:
             /* RMB DOWN CLICK */
-            
             //flag
             if(evt.target.classList.contains('flagged'))
             {
@@ -50,7 +83,6 @@ boardEl.addEventListener('mousedown', function(evt) {
                 setImg('closed_tile.png', evt)
                 flagCount--;
                 setBombDisplay();
-                
             } 
             //unflag
             else {
@@ -64,9 +96,8 @@ boardEl.addEventListener('mousedown', function(evt) {
         default:
             //console.log('You have a strange Mouse!');
     }
-    
+  
 })
-
 boardEl.addEventListener('mouseup', function(evt) {
     switch (evt.which) {
         case 1:
@@ -84,6 +115,9 @@ boardEl.addEventListener('mouseup', function(evt) {
             //console.log('You have a strange Mouse!');
     }
 })
+boardEl.addEventListener('contextmenu', function(evt) {
+    evt.preventDefault();
+  });
 
 // boardEl.addEventListener('mouseleave', function(evt) {
 //     console.log('mouseleave')
@@ -92,18 +126,15 @@ boardEl.addEventListener('mouseup', function(evt) {
 // })
 
 //get rid of the menu on right click
-boardEl.addEventListener('contextmenu', function(evt) {
-    evt.preventDefault();
-  });
+/*----- board listeners -----*
 
 
 /*----- functions -----*/
 
-
+//create the actual divs and images for the game
 function generateGrids(){
     for(let j=0; j<yAxis; j++){
         for (let i=0; i<xAxis; i++){
-            //TODO: fix the id generation
             let tile = document.createElement('div');
             tile.classList.add('tile')
             tile.classList.add('covered')
@@ -111,15 +142,18 @@ function generateGrids(){
             tile.style.backgroundColor = 'pink';
             tile.style.height = '30px';
             tile.style.width = '30px';
+            //helper function doesn't work here because it expects an event
             tile.style.background = `url('./assets/closed_tile.png')`;
             tile.style.backgroundSize = tileSize;
             document.querySelector('#board').appendChild(tile);    
-            
         }
     }
-    document.querySelector('#board').style.width = `${30*xAxis + 2*xAxis}px`; 
-    document.querySelector('#board').style.height = `${30*yAxis + 2*yAxis}px`;
-    document.querySelector('#statusBar').style.width = `${30*xAxis + 2*xAxis}px`;
+    //scale the board to fit the amount of tiles we have
+    document.querySelector('#board').style.width = `${32*xAxis}px`; 
+    document.querySelector('#board').style.height = `${32*yAxis}px`;
+
+    //TODO: Figure out what to do with the status bar
+    document.querySelector('#statusBar').style.width = `${32*xAxis}px`;
 
     let statusBarHeight;
     if(8*yAxis >= 90){
@@ -128,104 +162,29 @@ function generateGrids(){
         statusBarHeight = 90;
     }
     document.querySelector('#statusBar').style.height = `${statusBarHeight}px`;
-
 }
-
+//calculate the adjacentTiles and return an array of strings in the form of 'x,y'
 function adjacentTiles(coord){
-    //calculate the adjacent coordinates and return an array of the strings?
     let arr = []
     let temp = coord.split(',')
     let x = parseInt(temp[0], 10);
     let y = parseInt(temp[1], 10);
-
-    //if the coord is a corner
-    if((!x || x === xAxis-1) && (!y || y===yAxis-1)){
-
-      //top left corner
-      if(!x && !y){
-        arr.push(`${x+1},${y}`)       
-        arr.push(`${x},${y+1}`)
-        arr.push(`${x+1},${y+1}`)
-      }
-      //top right corner
-      else if(x===xAxis-1 && !y){
-        arr.push(`${x-1},${y}`)
-        arr.push(`${x-1},${y+1}`)
-        arr.push(`${x},${y+1}`)
-        
-      }
-      //bottom left corner
-      else if(!x && y===yAxis-1){
-        
-        arr.push(`${x},${y-1}`)
-        arr.push(`${x+1},${y-1}`)
-        arr.push(`${x+1},${y}`)
-        
-      }
-      //bottom right
-      //(x===xAxis-1 && y===yAxis-1)
-      else{
-        arr.push(`${x-1},${y-1}`)
-        arr.push(`${x},${y-1}`)
-        arr.push(`${x-1},${y}`)
-      }
-      
-    }
-    //if the coord is an edge and not a corner
-    else if((!x && y && y<yAxis-1) || (x===xAxis-1 && y && y<yAxis-1) || (!y && x && x<xAxis-1) || (y===yAxis-1 && x && x<xAxis-1)){
-      //top edge
-      if(!y && x && x<xAxis-1){
-        
-        arr.push(`${x-1},${y}`)
-        arr.push(`${x+1},${y}`)
-        
-        arr.push(`${x-1},${y+1}`)        
-        arr.push(`${x},${y+1}`)
-        arr.push(`${x+1},${y+1}`)
-      }
-      //bottom edge
-      else if(y===yAxis-1 && x && x<xAxis-1){
-        arr.push(`${x-1},${y-1}`)
-        arr.push(`${x},${y-1}`)
-        arr.push(`${x+1},${y-1}`)
-        arr.push(`${x-1},${y}`)
-        arr.push(`${x+1},${y}`)
-      }
-      //left edge
-      else if(!x && y && y<yAxis-1){
-        arr.push(`${x},${y-1}`)
-        arr.push(`${x+1},${y-1}`)
-        arr.push(`${x+1},${y}`)
-        arr.push(`${x},${y+1}`)
-        arr.push(`${x+1},${y+1}`)
-      }
-      //right edge
-      //x===xAxis-1 && y && y<yAxis-1
-      else{
-        arr.push(`${x-1},${y-1}`)
-        arr.push(`${x},${y-1}`)
-        arr.push(`${x-1},${y}`)
-        arr.push(`${x-1},${y+1}`)
-        arr.push(`${x},${y+1}`)
+  
+    for(let i=-1; i<2; i++){
+      if(((y+i)>=0) && (y+i)<yAxis){
+        for(let j=-1; j<2; j++){
+          //console.log("j, i", j, i)
+          if(((x+j)>=0 && (x+j)<xAxis) && !((y+i)===y && (x+j)===x)){
+            //console.log('pushed')
+            arr.push(`${x+j},${y+i}`)
+          }
+        }
       }
     }
-    //coord is not an edge
-    else{
-      //the 3 above
-      arr.push(`${x-1},${y-1}`)
-      arr.push(`${x},${y-1}`)
-      arr.push(`${x+1},${y-1}`)
-      //the 2 beside
-      arr.push(`${x-1},${y}`)
-      arr.push(`${x+1},${y}`)
-      //the 3 below
-      arr.push(`${x-1},${y+1}`)
-      arr.push(`${x},${y+1}`)
-      arr.push(`${x+1},${y+1}`)
-    }
-  return arr;
-}
-
+    return arr;
+  }
+//calculate the time to display and change out the images
+//note: requires help of setInterval()
 function clock(){
 
     if(seconds<1000)
@@ -245,7 +204,7 @@ function clock(){
     hundsEl.src = `./assets/d${hundreds}.svg`
 
 }
-
+//calculates the amount of bombs that are not flagged and change out the images
 function setBombDisplay(){
     //don't use actual bomb count here, don't want to mess with calculations
     let displayBombs = bombCount - flagCount
@@ -266,34 +225,65 @@ function setBombDisplay(){
     hundsEl.src = `./assets/d${hundreds}.svg`
 }
 
-//helper function
+
+//generates the coordinates of bombs based on bombCount
+//pushes the coordinates as strings in the form of 'x,y' to the global bombCoords array
+function generateBombs(){
+    //playing with bombs in corners
+    if(!noCornerMode){
+        while(bombCoords.length<bombCount){
+            let rand1 = Math.floor(Math.random() * (xAxis))
+            let rand2 = Math.floor(Math.random() * (yAxis))
+            bombCoords.push(`${rand1},${rand2}`);
+        }
+    } else{
+        //playing without bombs in corners
+        while(bombCoords.length<bombCount){
+            let rand1 = Math.floor(Math.random() * (xAxis))
+            let rand2 = Math.floor(Math.random() * (yAxis))
+            if((rand1!==0 && rand1!==xAxis-1) && rand2!==0 && rand2!==yAxis-1){
+                bombCoords.push(`${rand1},${rand2}`);
+            }
+        }
+    }
+}
+//helper functions
+//helper function to change the image given a string for the image and an event object
 function setImg(img, evt){
     evt.target.style.background = `url('./assets/${img}')`;
     evt.target.style.backgroundSize = tileSize;
 }
-function generateBombs(){
-    //playing with bombs in corners
-    if(!noCornerMode){
-        while(Object.keys(bombCoords).length<bombCount){
-            let rand1 = Math.floor(Math.random() * (xAxis))
-            let rand2 = Math.floor(Math.random() * (yAxis))
-            
-            bombCoords[`${rand1},${rand2}`] = true
-        }
-    }
+//clear the input boxes
+function clearInputEls(){
+    document.querySelector('#widthInput').value = '';
+    document.querySelector('#heightInput').value = '';
+    document.querySelector('#bombInput').value = '';
+}
+function reset(){
+    console.log('INIT CALLED')
+    clearInterval(refreshIntervalId);
+    //wow this is a neat way to remove all children tags
+    document.querySelector('#board').innerHTML = '';
+    // bombCount = 10;
+    // xAxis = 9;
+    // yAxis = 9;
+    seconds = 0;
 
-    //playing without bombs in corners
-    while(Object.keys(bombCoords).length<bombCount){
-        let rand1 = Math.floor(Math.random() * (xAxis))
-        let rand2 = Math.floor(Math.random() * (yAxis))
+    // newGame = false;
+}
+function init(){
+    //clear out old data
+    // if(newGame){
+    //     reset();
+    // }
+    reset();
 
-    if((rand1!==0 && rand1!==xAxis-1) && rand2!==0 && rand2!==yAxis-1)
-        bombCoords[`${rand1},${rand2}`] = true
-    }
+
+    
+    generateGrids();
+    generateBombs();
+    setBombDisplay();
+    refreshIntervalId = setInterval(clock, 1000);
 }
 //GAME START
-generateGrids();
-
-let refreshIntervalId = setInterval(clock, 1000);
-//clearInterval(refreshIntervalId);
-
+init();
